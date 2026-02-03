8837df47338949156bda08120b9e80b7e7b2de7b
CMP_DaLysychenko <danylollysychenko@gmail.com>
2026-02-03 09:40:02 -0500

    done i guess

--- driver.py
+++ driver.py
@@ -2,9 +2,13 @@ import app as dr
 from app import Player
 import os
 
-round = 1
-player_index = 0
-num_of_players = int(input("Enter # of people playing -> "))
+while True:
+    try:
+        num_of_players = int(input("Enter # of people playing -> "))
+        break
+    except ValueError:
+        print("Invalid input.\nTry again.")
+
 for i in range(num_of_players):
     name = input("Enter your name:").title().strip()
     player = Player(name)
@@ -23,86 +27,89 @@ roller = dr.Dice_Roller()
 # while found_winner = false
 # for each player - loop until the player's turn ends (stop or farkle)
 # after each players turn, check for winner and set flag if won
-found_winner = True
+found_winner = False
 round_score = 0
-while found_winner:
+round = 1
+while not found_winner:
     for player in Player.players:
-        print(player.name)
+        #print(player.name)
         # have a function to handle the players turn
         # player.score += play_round(player, dice)
         # if player.score >= target_score:
         # found_winner = True #set flag to exit loop
 
 # find who had the highest score, and declare as winner
-
-    exit()
-    try:
         print()
-        print(f"Round {round}.\n{player.players[player_index]}'s turn.")
+        print(f"Round {round}.\n{player.name}'s turn.")
         print(
-            f"{player.players[player_index]} do you wish to (roll or bank) your points?: ")
-        print("Please enter \"r\" to roll or \"b\" to bank: ")
-        choice = input("Choice -> ").lower().strip()
-        if choice == "r":
-            roller.roll()
-            roller.print_dice()
-            round_score += roller.determine_points()
-            print(
-                f"{player.players[player_index]} has {round_score} points so far this round.")
-
-            if roller.determine_points() == 0:
-                print(f"{player.players[player_index]} have farkled.")
+            f"{player.name} do you wish to (roll or bank) your points?: ")
+        while True:
+            print("Please enter \"r\" to roll or \"b\" to bank: ")
+            choice = input("Choice -> ").lower().strip()
+            if choice == "r":
+                roller.roll()
+                roller.print_dice()
+                round_score += roller.determine_points()
                 print(
-                    f"{player.players[player_index]} gets 0 points for this round.")
-                player.score -= round_score
-                player_index += 1
+                    f"{player.name} has {round_score} points so far this round.")
+                print()
 
-        elif choice == "b":
-            player.score += round_score
-            print(f"{player.players[player_index]} has {player.score} points.")
-            player_index += 1
-            round_score = 0
+                if roller.determine_points() == 0:
+                    print(f"{player.name} have farkled.")
+                    print(
+                        f"{player.name} gets 0 points for this round.")
+                    print()
+                    player.score -= round_score
+                    break
 
+            if choice == "b":
+                player.score += round_score
+                print(f"{player.name} has {player.score} points.")
+                round_score = 0
+                break
+    
         if player.score >= score_goal:
-            print(f"{player.players[player_index]} has won the game!")
-            found_winner = False
+            print(f"{player.name} reached the score goal.")
+            winner = Player.players[0]
+            for player in Player.players:
+                if player.score > winner.score:
+                    winner = player
+                    print(f"Winner is {winner.name} with {winner.score} points!")
+            found_winner = True
 
-        if num_of_players == 1 and player.score == score_goal:
-            print(f"{player.players} has won!")
-            exit()
+        if num_of_players == 1 and player.score >= score_goal:
+            print(f"{player.name} has won!")
+            found_winner = True
 
-    except IndexError:
-        player_index = 0
-        if player_index == 0:
-            round += 1
+    round += 1
 
-try:
-    player_index += 1
-    print(f"Last chance {player.players[player_index]}")
-    print(f"You have {player.score} right now.")
-    print(
-        f"{player.players[player_index]} do you wish to (roll or bank) your points?: ")
-    print("Please enter \"roll\" or \"bank\" ")
-    choice = input("Choice -> ")
-    if choice == "roll":
-        roller.roll()
-        roller.print_dice()
-        player.score += roller.determine_points()
-        print(f"{player.players[player_index]} has {player.score} points.")
+# try:
+#     player_index += 1
+#     print(f"Last chance {player.players[player_index]}")
+#     print(f"You have {player.score} right now.")
+#     print(
+#         f"{player.players[player_index]} do you wish to (roll or bank) your points?: ")
+#     print("Please enter \"roll\" or \"bank\" ")
+#     choice = input("Choice -> ")
+#     if choice == "roll":
+#         roller.roll()
+#         roller.print_dice()
+#         player.score += roller.determine_points()
+#         print(f"{player.players[player_index]} has {player.score} points.")
 
-        if roller.determine_points() == 0:
-            print(f"{player.players[player_index]} have farkled.")
-            print(
-                f"{player.players[player_index]} get 0 points for this round.")
-            player.score = 0
-            player_index += 1
+#         if roller.determine_points() == 0:
+#             print(f"{player.players[player_index]} have farkled.")
+#             print(
+#                 f"{player.players[player_index]} get 0 points for this round.")
+#             player.score = 0
+#             player_index += 1
 
-    elif choice == "bank":
-        print(f"{player.players[player_index]} has {player.score} points.")
-        player_index += 1
+#     elif choice == "bank":
+#         print(f"{player.players[player_index]} has {player.score} points.")
+#         player_index += 1
 
-except IndexError:
-    player_index = 0
+# except IndexError:
+#     player_index = 0
 
 # roller.roll()
 # roller.print_dice()
